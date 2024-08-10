99dd4ade494819f4b724f2745b5a1a927e911e57
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-08-10 22:04:07 +0400

    ?

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import { createContext, useState } from "react";
 import { TcontextType } from "./types/ContextType";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import AllCategory from "./pages/AllCategory"
+import AllCategory from "./pages/AllCategory";
 
 import { useMediaQuery } from "@uidotdev/usehooks";
 import LargeHeader from "./components/LargeHeader";
+import SingleCategory from "./pages/SingleCategory";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 export const Context = createContext<TcontextType>({
   burgerToShow: false,
   setBurgerToShow: () => {},
-test: false,
-  setTest: () => {}})
-
+});
 
 function App() {
   const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
@@ -22,25 +23,13 @@ function App() {
   return (
     <Context.Provider value={{ burgerToShow, setBurgerToShow }}>
       <BrowserRouter>
-
         {isSmallDevice ? <Header /> : <LargeHeader />}
         <Routes>
           <Route path="/" element={<Home />} />
-
-     
-        <Header />
-        
-        <Routes>
-          <Route path="/" 
-                 element={<Home />
-                } 
-              />
-
-          <Route path="/all-categories-products" 
-                 element={<AllCategory />
-                } 
-              />
-
+          <Route path="/all-categories-products" element={<AllCategory />} />
+          <Route path="/singleCategory" element={<SingleCategory />} />
+          <Route path="signUp" element={<SignUp />} />
+          <Route path="Login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
