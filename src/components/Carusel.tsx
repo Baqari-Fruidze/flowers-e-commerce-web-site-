366b2bdeea218150359f915bedfd1aca872eeba3
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-08-21 15:54:10 +0400

    carusel done,litle changes needed

--- src/components/Carusel.tsx
+++ src/components/Carusel.tsx
@@ -1,29 +1,54 @@
 import React from "react";
 import data from "../data.json";
 import styled from "styled-components";
-import Splide from "@splidejs/splide";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 export default function Carusel() {
   return (
     <Parent>
       <Title>Excellent Combination with:</Title>
       <CaruselCon>
-        {data.datas[7].carusel?.map((item) => {
-          return (
-            <SingleItemCon>
-              <Image src={item.src} alt="" />
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <p>{item.name}</p>
-                <p>${item.price}</p>
-              </div>
-            </SingleItemCon>
-          );
-        })}
+        <Splide
+          options={{
+            width: 300,
+            type: "loop",
+            autoplay: false,
+            rewind: true,
+            pagination: false,
+            arrows: false,
+            gap: "3rem",
+            intervall: 1000,
+          }}
+        >
+          {data.datas[7].carusel?.map((item) => {
+            return (
+              <SplideSlide>
+                <SingleItemCon>
+                  <Image src={item.src} alt="" />
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <NameP>{item.name}</NameP>
+                    <PriceP>${item.price}</PriceP>
+                  </div>
+                </SingleItemCon>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
       </CaruselCon>
     </Parent>
   );
 }
+const PriceP = styled.p`
+  text-align: center;
+  color: var(--Gray, #808080);
+  font-family: Gilroy;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%;
+`;
 const NameP = styled.p`
+  text-align: center;
   color: var(--Black, #121212);
   font-family: Gilroy;
   font-size: 14px;
@@ -39,9 +64,17 @@ const SingleItemCon = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
+  .splide__slide is-next {
+    list-style-type: none;
+  }
 `;
 const CaruselCon = styled.div`
   display: flex;
+  overflow-x: hidden;
+  width: 3orem;
+  & .splide__list {
+    display: flex;
+  }
 `;
 const Title = styled.h1`
   color: var(--Black, #121212);
