1e791eaaacb7e752c8519a81c6db665b1b509293
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-09-02 21:08:30 +0400

    recover

--- src/pages/Restore.tsx
+++ src/pages/Restore.tsx
@@ -1,30 +1,157 @@
-import { useContext } from "react";
+import { useCallback, useState } from "react";
 import styled from "styled-components";
-import { Context } from "../App";
 import { useNavigate } from "react-router-dom";
+import { boolean } from "yup";
 
 export default function Restore() {
-  const { recoverUsername, setRecoverUsername } = useContext(Context);
+  const [first, setFirst] = useState<boolean>(false);
+  const [second, setSecond] = useState<boolean>(false);
+  const [third, setThird] = useState<boolean>(true);
+  const [username, setUsarname] = useState<string>("");
+  const [recoverPassword, setRecoverPassword] = useState<string>("");
+  const [recoverCode, setRecoverCode] = useState<string>("");
   const navigate = useNavigate();
+  async function userCheck() {
+    console.log(username);
+    const res = await fetch("http://134.122.71.97:8000/auth/recover", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username }),
+    });
+
+    if (res.ok) {
+      setFirst(true);
+      setThird(false);
+    }
+  }
+  async function recoveryPassword() {
+    const res = await fetch("http://134.122.71.97:8000/auth/recover", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ recovery_code: recoverCode }),
+    });
+    if (res.ok) {
+      setFirst(false);
+      setSecond(true);
+    }
+  }
+  async function changePassword() {
+    const res = await fetch("http://134.122.71.97:8000/auth/recover", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ recovery_code: recoverCode }),
+    });
+    if (res.ok) {
+      setFirst(false);
+      setSecond(true);
+    }
+  }
 
   return (
     <Parent>
       <InsideCon>
-        <Label htmlFor="username">please Enter Username</Label>
-        <Input
-          type="text"
-          placeholder="username"
-          id="username"
-          onChange={(e) => setRecoverUsername(e.target.value)}
-          value={recoverUsername}
-        />
+        {third ? (
+          <SingleCon>
+            <Label htmlFor="username">please Enter Username</Label>
+            <Input
+              type="text"
+              placeholder="username"
+              id="username"
+              onChange={(e) => setUsarname(e.target.value)}
+              value={username}
+              // onKeyDown={(e) => {
+              //   if (e.key === "Enter" && username.length > 0) {
+              //     userCheck();
+              //   }
+              // }}
+            />
+            <Btn
+              onClick={() => {
+                if (username.length > 0) {
+                  userCheck();
+                }
+              }}
+            >
+              Send
+            </Btn>
+          </SingleCon>
+        ) : null}
+        {first ? (
+          <SingleCon>
+            <Label htmlFor="recover">Enter Recovery Code</Label>
+            <RecoverInput
+              type="text"
+              id="recover"
+              placeholder="6 chars"
+              value={recoverCode}
+              onChange={(e) => setRecoverCode(e.target.value)}
+              // onKeyDown={(e) => {
+              //   if (e.key === "Enter" && recoverCode.length === 6) {
+              //     recoveryPassword();
+              //   }
+              // }}
+            />
+            <Btn
+              onClick={() => {
+                if (recoverCode.length === 6) {
+                  recoveryPassword();
+                }
+              }}
+            >
+              Send
+            </Btn>
+          </SingleCon>
+        ) : null}
+        {second ? (
+          <SingleCon>
+            <Label htmlFor="password">Enter New Password</Label>
+            <RecoverInput
+              type="text"
+              id="password"
+              placeholder="Password"
+              value={recoverPassword}
+              onChange={(e) => setRecoverPassword(e.target.value)}
+            />
+            <Btn
+              onClick={() => {
+                if (recoverPassword.length > 3) {
+                }
+              }}
+            >
+              Send
+            </Btn>
+          </SingleCon>
+        ) : null}
+
         <p>(your recovery code has been sent to your email)</p>
-        <Btn onClick={() => navigate("/login")}>Recover</Btn>
       </InsideCon>
     </Parent>
   );
 }
 
+const RecoverInput = styled.input`
+  width: 100%;
+  padding: 1.5rem 0 1.5rem 2.5rem;
+  border: none;
+  font-size: 22px;
+  border-bottom: 1px solid #121212;
+  &::placeholder {
+    font-size: 22px;
+    color: grey;
+    padding-left: 1rem;
+  }
+`;
+const SingleCon = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
 const Btn = styled.button`
   padding: 1.5rem;
   font-size: 16px;
@@ -32,6 +159,7 @@ const Btn = styled.button`
   background-color: #121212;
   border: none;
   border-radius: 8px;
+  margin-top: 1.6rem;
 `;
 const Input = styled.input`
   width: 100%;
@@ -42,12 +170,12 @@ const Input = styled.input`
   &::placeholder {
     font-size: 22px;
     color: grey;
-    padding-left: 2rem;
+    padding-left: 1rem;
   }
 `;
 const Label = styled.label`
   color: var(--Black, #121212);
-  text-align: center;
+  text-align: left;
   font-family: "BIZ UDGothic";
   font-size: 24px;
   font-style: normal;
@@ -61,7 +189,7 @@ const InsideCon = styled.div`
   background-color: #fff;
   display: flex;
   flex-direction: column;
-  gap: 3.4rem;
+  gap: 2.4rem;
   max-width: 38rem;
 `;
 const Parent = styled.div`
