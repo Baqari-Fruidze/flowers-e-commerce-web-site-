53fa301c9974b115e7a5bcb9ed9b9603da2097d9
KatrinaZuus <katrinazuus@gmail.com>
2024-09-14 16:27:46 +0400

    try

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,8 +22,6 @@ export default function Login() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        // Authorization:
-        //   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI0NDMzNDc0LCJpYXQiOjE3MjQ0M",
       },
       body: JSON.stringify(userinfo),
     });
